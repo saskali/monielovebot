047954bb784406209dc0c5def6ab1030e0eac795
Fabrizio Ferrai <fabrizio.ferrai@gmail.com>
2020-03-07 21:33:51 +0100

    Move to jsonl and add an audit log

--- spago.dhall
+++ spago.dhall
@@ -1,14 +1,16 @@
 { name = "monielovebot"
 , dependencies =
-    [ "console"
-    , "effect"
-    , "node-fs"
-    , "node-telegram-bot-api"
-    , "numbers"
-    , "psci-support"
-    , "simple-json"
-    , "string-parsers"
-    ]
+  [ "console"
+  , "effect"
+  , "kishimen"
+  , "node-fs"
+  , "node-telegram-bot-api"
+  , "numbers"
+  , "psci-support"
+  , "simple-json"
+  , "string-parsers"
+  , "stringutils"
+  ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs" ]
 }
--- src/Main.purs
+++ src/Main.purs
@@ -12,12 +12,16 @@ import Data.List as List
 import Data.Maybe (Maybe(..))
 import Data.Number as Numbers
 import Data.String.CodeUnits (fromCharArray)
+import Data.String.Utils as String
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
 import Effect (Effect)
 import Effect.Class.Console (logShow)
 import Effect.Console (log)
+import Kishimen (genericSumToVariant, variantToGenericSum)
 import Node.Encoding (Encoding(..))
 import Node.FS.Sync as FS
-import Simple.JSON (readJSON, writeJSON)
+import Simple.JSON as Json
 import TelegramBot as Telegram
 import Text.Parsing.StringParser (ParseError, fail, runParser)
 import Text.Parsing.StringParser.CodePoints (anyChar, anyDigit, satisfy, skipSpaces, string)
@@ -28,12 +32,20 @@ type Config =
   , usernames :: Array String
   }
 
+-- | An entry of the "transaction log"
 type Transaction =
   { amount :: Number
   , reason :: String
   , issuer :: String
   , date :: Int
   }
+
+-- | An entry of the "command log"
+type Entry =
+  { command :: Command
+  , timestamp :: Int
+  }
+
 
 -- | This holds the reference to our current bot/chat around the app,
 -- | so we can send messages back
@@ -52,26 +64,33 @@ data Command
   -- | prints out log of all transactions
   | Log
 
+
+derive instance genericCommand :: Generic Command _
+
 instance showCommand :: Show Command where
-  show Balance = "Balance"
-  show Payout = "Payout"
-  show (Add transaction) = "Add " <> show transaction
-  show Log = "Log"
+  show = genericShow
+
+instance writeForeignCommand :: Json.WriteForeign Command where
+  writeImpl = genericSumToVariant >>> Json.writeImpl
+
+instance readForeignCommand :: Json.ReadForeign Command where
+  readImpl = map variantToGenericSum <<< Json.readImpl
+
 
 {-
 
 TODO:
 - answer to balance command
-- write audit log of commands
 
 -}
 
-transactionFile :: String
-transactionFile =  "./transactions.json"
+commandLog :: String
+commandLog = "commandlog.jsonl"
+
 
 main :: Effect Unit
 main = do
-  c <- map readJSON (FS.readTextFile UTF8 "./config.json")
+  c <- map Json.readJSON (FS.readTextFile UTF8 "./config.json")
   case c of
     Left e ->
       log $ "config.json is malformed: " <> show e
@@ -87,23 +106,32 @@ main = do
           , Just user <- Array.find (username == _) config.usernames
           , Just text <- message.text = case parseCommand text user message.date of
             Right command -> do
-              log $ "Running command: " <> show command
-              runCommand { bot, chatID: message.chat.id } command
+              runCommand { bot, chatID: message.chat.id } { command, timestamp: message.date }
             Left errors -> logShow errors
           | otherwise = pure unit
 
-withTransactions :: (Array Transaction -> Effect Unit) -> Effect Unit
-withTransactions action = do
-  whenM (map not $ FS.exists transactionFile) do
-    log "Transaction file doesn't exist, creating.."
-    FS.writeTextFile UTF8 transactionFile "[]"
-  maybeTransactions <- map readJSON (FS.readTextFile UTF8 transactionFile)
-  case maybeTransactions of
-    Left e -> log $ "ERROR: transactions.json is malformed: " <> show e
-    Right transactions -> action transactions
 
-runCommand :: TelegramCoords -> Command -> Effect Unit
-runCommand telegram command = withTransactions \transactions -> do
+-- | Read and parse the Json-Lines log of commands into memory
+readJsonlFile :: forall a. Json.ReadForeign a => String -> Effect (Array a)
+readJsonlFile filename = do
+  lines <- map String.lines $ FS.readTextFile UTF8 filename
+  pure $ Array.mapMaybe parseLine lines
+  where
+    parseLine line = case Json.readJSON line of
+      Left _ -> Nothing
+      Right e -> e
+
+
+runCommand :: TelegramCoords -> Entry -> Effect Unit
+runCommand telegram entry = do
+  -- First, let's print the command out
+  log ("New command: " <> show entry.command)
+  -- Then append it to the audit log
+  FS.appendTextFile UTF8 commandLog (Json.writeJSON entry <> "\n")
+  -- At this point we're safe to read all the entries from the command log
+  entries <- readJsonlFile commandLog
+  let transactions = transactionsFromLog entries
+
   let send msg = Telegram.sendMessageWithOptions telegram.bot telegram.chatID msg { parse_mode: "Markdown" }
   let getUserBalance userTransactions =
         { issuer: _.issuer $ NonEmpty.head userTransactions
@@ -121,7 +149,7 @@ runCommand telegram command = withTransactions \transactions -> do
           in "`" <> mostSpender.issuer <> "` spent " <> show diff <> " more than `" <> leastSpender.issuer <> "`"
         _ -> "Stuff's broken yo"
 
-  case command of
+  case entry.command of
     Balance ->
       send $ "Current balance:\n" <> Array.fold (map formatBalance $ getBalances transactions)
 
@@ -131,13 +159,10 @@ runCommand telegram command = withTransactions \transactions -> do
           let diff = mostSpender.total - leastSpender.total
           send $ "Current balance:\n" <> Array.fold (map formatBalance $ getBalances transactions)
           send $ "`" <> leastSpender.issuer <> "` owes " <> show (diff / 2.0) <> " to `" <> mostSpender.issuer <> "`"
-          FS.unlink transactionFile
+          FS.rename commandLog (commandLog <> ".bak")
         _ -> send "Stuff's broken yo"
 
     Add transaction -> do
-      log "Writing new transaction to file.."
-      let newTransactions = Array.cons transaction transactions
-      FS.writeTextFile UTF8 transactionFile $ writeJSON newTransactions
       send $ "You have added " <> show transaction.amount <> " for " <> show transaction.reason <> "\n" <> differenceText
 
     Log -> do
@@ -148,8 +173,17 @@ formatLog :: Array Transaction -> String
 formatLog transactions = foldMap formatTransaction transactions
   where
     formatTransaction :: Transaction -> String
-    formatTransaction {issuer, amount, reason, date} 
+    formatTransaction {issuer, amount, reason, date}
       = "- " <> issuer <> " spent " <> show amount <> "€ for " <> reason <> " on the " <> show date <> "\n"
+
+
+transactionsFromLog :: Array Entry -> Array Transaction
+transactionsFromLog = Array.mapMaybe fromEntry
+  where
+    fromEntry { command } = case command of
+      Add transaction -> Just transaction
+      _ -> Nothing
+
 
 parseCommand :: String -> String -> Int -> Either ParseError Command
 parseCommand text username timestamp = do
